9bbc1280909996e757cd1f1d4693b716c6f9faec
Nobuyuki Takahiro <studio.quad9@gmail.com>
2021-02-14 18:32:04 +0900

    book base build

---
+++ vivliostyle.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: 'css_syntax', // populated into `manifest.json`, default to `title` of the first entry or `name` in `package.json`.
+  author: '高広信之 <takahiro@kumihan.com>', // default to `author` in `package.json` or undefined.
+  // language: 'ja', // default to `en`.
+  // size: 'A4', // paper size.
+  theme: 'i-theme/theme.css', // .css or local dir or npm package. default to undefined.
+  // entryContext: './manuscripts', // default to '.' (relative to `vivliostyle.config.js`).
+  entry: [
+    '_sf_columns.md', // `title` is automatically guessed from the file (frontmatter > first heading).
+    // {
+    //   path: 'epigraph.md',
+    //   title: 'Epigraph', // title can be overwritten (entry > file),
+    //   theme: '@vivliostyle/theme-whatever', // theme can be set indivisually. default to the root `theme`.
+    // },
+    // 'glossary.html', // html can be passed.
+  ], // `entry` can be `string` or `object` if there's only single markdown file.
+  // toc: true, // whether generate and include toc.html or not (does not affect manifest.json), default to `true`. if `string` given, use it as a custom toc.html.
+  // cover: './cover.png', // cover image. default to undefined.
+  // workDir: './dist', // default to `.vivliostyle`.
+}
