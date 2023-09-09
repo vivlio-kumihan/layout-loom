2683b15b92a5f1c03bd8f01107243583e13370f1
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-09-09 09:29:16 +0900

    read code ScrollingHeader.js

--- src/Module/ScrollingHeader.js
+++ src/Module/ScrollingHeader.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import styled from 'styled-components';
 
+// style定義 ///////////////////////////////////////
 const StyleScrollingHeader = styled.header`
   display: flex;
   justify-content: space-between;
@@ -36,32 +37,47 @@ const Anc = styled.a`
   cursor: pointer;
 `
 
+// ScrollingHeaderコンポーネント定義 ///////////////////////////
 const ScrollingHeader = () => {
+  // このコンポーネントはuseScrollDirection関数の実行
   const useScrollDirection = () => {
+    // className={`scroll-${scrollDirection}`}にdown, upを入れる。
     const [scrollDirection, setScrollDirection] = useState('');
+    // スクリーン上辺からの距離を監視
     const [prevOffset, setPrevOffset] = useState(0);
 
-    const toggleScrollDirection = () => {
-      let scrollY = window.scrollY;
-      if (scrollY > prevOffset && scrollY > 50) {
-        setScrollDirection('down');
-      } else if (scrollY < prevOffset && scrollY > 50) {
-        setScrollDirection('up');
-      } else {
-        setScrollDirection('');
-      }
-      setPrevOffset(scrollY);
-    };
-
     useEffect(() => {
+      const toggleScrollDirection = () => {
+        let scrollY = window.scrollY;
+        console.log(scrollY, "scrollY")
+        console.log(prevOffset, "prevOffset")
+        if (scrollY > prevOffset && scrollY > 50) {
+          setScrollDirection('down');
+        } else if (scrollY < prevOffset && scrollY > 50) {
+          setScrollDirection('up');
+        } else {
+          setScrollDirection('');
+        }
+        setPrevOffset(scrollY);
+      };
+
+      // スクロールしている状態では、toggleScrollDirection関数を呼び出し、
       window.addEventListener('scroll', toggleScrollDirection);
+
+      // スクロールを止めた時が、useScrollDirection関数の戻り値となる。
+      // というか、まだなんとなくしか解析できてない。
+      // でも状態を管理しているんだと感じれて楽しい。
+      // まだ、useState止まりで勉強中。次に進みたい。230909sat
       return () => {
-        window.removeEventListener('scroll', toggleScrollDirection)
+        window.removeEventListener('scroll', toggleScrollDirection);
       };
     }, [prevOffset]);
+
+    // downまたはupを送る（className={`scroll-${scrollDirection}`}に向けて）。
     return scrollDirection;
   };
 
+  // useScrollDirection関数を実行してJSXへ送信 /////////////////
   const scrollDirection = useScrollDirection();
 
   return (
@@ -86,4 +102,68 @@ const ScrollingHeader = () => {
 };
 
 export default ScrollingHeader;
+
+// const ScrollingHeader = () => {
+//   const useScrollDirection = () => {
+//     const [scrollDirection, setScrollDirection] = useState('');
+//     const [prevOffset, setPrevOffset] = useState(0);
+
+//     const toggleScrollDirection = () => {
+//       let scrollY = window.scrollY;
+//       if (scrollY > prevOffset && scrollY > 50) {
+//         setScrollDirection('down');
+//       } else if (scrollY < prevOffset && scrollY > 50) {
+//         setScrollDirection('up');
+//       } else {
+//         setScrollDirection('');
+//       }
+//       setPrevOffset(scrollY);
+//     };
+
+//     useEffect(() => {
+//       const toggleScrollDirection = () => {
+//         let scrollY = window.scrollY;
+//         if (scrollY > prevOffset && scrollY > 50) {
+//           setScrollDirection('down');
+//         } else if (scrollY < prevOffset && scrollY > 50) {
+//           setScrollDirection('up');
+//         } else {
+//           setScrollDirection('');
+//         }
+//         setPrevOffset(scrollY);
+//       };
+
+//       window.addEventListener('scroll', toggleScrollDirection);
+//       return () => {
+//         window.removeEventListener('scroll', toggleScrollDirection);
+//       };
+//     }, [prevOffset]);
+
+//     return scrollDirection;
+//   };
+
+//   const scrollDirection = useScrollDirection();
+
+//   return (
+//     <StyleScrollingHeader className={`scroll-${scrollDirection}`}>
+//         <h1>scrolling header</h1>
+//         <Ul role="navigation">
+//           <li>
+//             <Anc>Link 1</Anc>
+//           </li>
+//           <li>
+//             <Anc>Link 1</Anc>
+//           </li>
+//           <li>
+//             <Anc>Link 2</Anc>
+//           </li>
+//           <li>
+//             <Anc>Link 3</Anc>
+//           </li>
+//         </Ul>
+//     </StyleScrollingHeader>
+//   );
+// };
+
+// export default ScrollingHeader;
 
